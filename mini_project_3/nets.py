4c87c51eb1b6730c050eb048332a6bca2aa007f7
A-Rozmarynowicz <arkadiuszrozmarynowicz@gmail.com>
2026-04-17 11:23:55 +0200

    Troche grubsza siec

--- mini_project_3/nets.py
+++ mini_project_3/nets.py
@@ -16,32 +16,41 @@ from tqdm import tqdm
 
 
 class Net(nn.Module):
-    def __init__(self, kernel_sizes=[5, 2, 2], n_channels=6, num_classes=50, hidden_size=25):
+    def __init__(self, num_classes=10):
         super().__init__()
-        ## Warstwa konwolucyjna
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=n_channels, kernel_size=kernel_sizes[0], stride=1, padding=0)
-        ## Warstwa max pooling
-        self.pool1 = nn.MaxPool2d(kernel_size=kernel_sizes[1], stride=2)
-        self.conv2 = nn.Conv2d(n_channels, 16, kernel_size=kernel_sizes[2])
-        self.pool2 = nn.MaxPool2d(kernel_size=kernel_sizes[2], stride=2)
-        # !!!
-        self.fc1 = nn.Linear(3136, hidden_size)
-        # self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, num_classes)
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, 3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(64, num_classes)
+        )
 
     def forward(self, x):
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
-        # !!!
-        x = torch.flatten(x, start_dim=1) # flatten all dimensions except batch
-        # print(x.size())
-        # !!!
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-
-        return x
+        x = self.features(x)
+        x = self.gap(x)
+        x = self.classifier(x)
+        return x  
 
 # skopiowane z lab6:
 # trzeba zamineć kernel_size na kernel_sizes i dodać hidden_size do argumentów konstruktora, a potem przekazać je do warstwy fc1, która musi być LazyLinear, bo nie znamy rozmiaru wejścia (zależy on od rozmiaru obrazu i kernel_sizes)
