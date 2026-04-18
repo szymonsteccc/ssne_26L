869e812f0b6cd6d4ede4f7ecdd097044c3904e15
A-Rozmarynowicz <arkadiuszrozmarynowicz@gmail.com>
2026-04-18 21:30:27 +0200

    Koniec

---
+++ mini_project_3/piatek_RozmarynowiczArkadiusz_StecSzymon/nets.py
@@ -0,0 +1,63 @@
+import torch
+import torchvision
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+import torch.nn as nn
+import torch.nn.functional as F
+from collections import Counter
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import torch.optim as optim
+from tqdm import tqdm
+
+
+
+class Net(nn.Module):
+    ## Tu wcześniej były parametryzowane rozmiary kerneli itp, ale ostatecznie taka była najlepsza
+    def __init__(self, num_classes=50):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, 3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128, 256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.gap(x)
+        x = self.classifier(x)
+        return x
---
+++ mini_project_3/piatek_RozmarynowiczArkadiusz_StecSzymon/train.py
@@ -0,0 +1,136 @@
+import numpy as np
+import torch
+import torchvision
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+import torch.nn as nn
+import torch.nn.functional as F
+from collections import Counter
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import torch.optim as optim
+from tqdm import tqdm
+
+from nets import *
+
+
+def train_model(net, train_loader, val_loader, criterion, optimizer, eval_func, device, num_epochs=5, verbose=True):
+    train_eval_hist = []
+    val_eval_hist = []
+    loss_hist = []
+
+    rng = tqdm(range(num_epochs)) if verbose else range(num_epochs)
+
+    total = 0
+    correct = 0
+
+    # Przetrenowanie
+    best_val_acc = 0
+    max_plateau = 5
+    plateau_counter = 0
+
+    for epoch in rng:
+        net.train()
+        running_loss = 0.0
+        total = 0
+        correct = 0
+
+        for data in train_loader:
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            with torch.no_grad():
+              preds = outputs.argmax(dim=1)
+
+            correct += (preds == labels).sum().item()
+            total += labels.size(0)
+
+            running_loss += loss.item()
+
+        epoch_loss = running_loss / len(train_loader)
+        loss_hist.append(epoch_loss)
+
+        train_acc = correct / total
+        train_eval_hist.append(train_acc)
+        val_eval_hist.append(eval_func(net, val_loader, device))
+
+        if not verbose:
+            print('[%d/%d] loss: %.3f' % (epoch + 1, num_epochs, epoch_loss))
+        else:
+            rng.set_postfix(
+                loss=f"{epoch_loss:.3f}",
+                train_acc=f"{train_acc:.3f}",
+                val_acc=f"{val_eval_hist[-1]:.3f}"
+            )
+            if (epoch+1) % 1 == 0:
+              plot_training_chart(loss_hist, train_eval_hist, val_eval_hist)
+
+        val_acc = val_eval_hist[-1]
+
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            plateau_counter = 0
+        else:
+            plateau_counter += 1
+
+        if plateau_counter >= max_plateau:
+            print("Early stopping: validation accuracy stopped improving")
+            break
+
+    if not verbose:
+        print('Finished Training')
+
+    return loss_hist, train_eval_hist, val_eval_hist
+
+
+def plot_training_chart(loss_hist, train_eval_hist, val_eval_hist):
+    fig, ax1 = plt.subplots()
+
+    color = 'tab:red'
+    ax1.set_xlabel('epoch')
+    ax1.set_ylabel('loss', color=color)
+    ax1.plot(loss_hist, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()
+
+    color = 'tab:blue'
+    ax2.set_ylabel('accuracy', color=color)
+    ax2.plot(train_eval_hist, label="train", color="blue")
+    ax2.plot(val_eval_hist, label="val", color="cyan")
+    ax2.tick_params(axis='y', labelcolor=color)
+    fig.legend(loc="upper right", bbox_to_anchor=(0.9, 0.9))
+    fig.tight_layout()
+    plt.show()
+
+
+def calc_accuracy(predictions: np.ndarray, targets: np.ndarray, n_classes=50):
+    assert len(predictions) == len(targets)
+    accuracies = []
+    for i in range(n_classes):
+        accuracies.append((predictions[targets == i] == i).sum() / (targets == i).sum())
+    return np.mean(accuracies)
+
+
+def get_accuracy(model, data, device):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in data:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs, dim=1)
+            total += labels.shape[0]
+            correct += int((predicted == labels).sum())
+    return correct / total
+
