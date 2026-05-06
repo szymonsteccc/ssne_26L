42af6ea2179d8bd7ed2bd24691057d33d5ed1959
A-Rozmarynowicz <arkadiuszrozmarynowicz@gmail.com>
2026-05-06 16:09:18 +0200

    Normalizing

---
+++ generatywne/modele.py
@@ -0,0 +1,51 @@
+## PyTorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+import torch.optim as optim
+# Torchvision
+import torchvision
+from torchvision.datasets import FashionMNIST
+from torchvision import transforms
+
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
+from torch.utils.data import random_split
+
+
+class BasicUNet(nn.Module):
+    """A minimal UNet implementation."""
+    def __init__(self, in_channels=1, out_channels=1):
+        super().__init__()
+        self.down_layers = torch.nn.ModuleList([
+            nn.Conv2d(in_channels, 32, kernel_size=5, padding=2),
+            nn.Conv2d(32, 64, kernel_size=5, padding=2),
+            nn.Conv2d(64, 64, kernel_size=5, padding=2),
+        ])
+        self.up_layers = torch.nn.ModuleList([
+            nn.Conv2d(64 + 1, 64, kernel_size=5, padding=2), #Dodajmy warunkowanie "krokiem odszumiania"
+            nn.Conv2d(64, 32, kernel_size=5, padding=2),
+            nn.Conv2d(32, out_channels, kernel_size=5, padding=2),
+        ])
+        self.act = nn.SiLU() # The activation function
+        self.downscale = nn.MaxPool2d(2)
+        self.upscale = nn.Upsample(scale_factor=2)
+
+    def forward(self, x, t):
+        h = []
+        for i, layer in enumerate(self.down_layers):
+            x = self.act(layer(x))
+            if i < 2: # Dla wszystki warstw "down" poza ostatnim
+              h.append(x) # Zapisujemy "skip-connetions"
+              x = self.downscale(x) # Zmniejszamy wymiarowość, i propagujemy do kolejnej warstwy
+        t = t.repeat(1,x.size(2),x.size(3),1).permute(3,0,1,2)
+        x = torch.cat([x,t],dim=1)
+        for i, layer in enumerate(self.up_layers):
+            if i > 0: # Dla wszystkich warstw up poza pierwszą
+              x = self.upscale(x) # Upscale
+              x += h.pop() # Dodajemy zapisane skip-connection
+            x = self.act(layer(x))
+
+        return x
