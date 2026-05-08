6707dab9ab138654c403873522d12a664213bafc
szymonstec <szymonsteccc@gmail.com>
2026-05-08 15:09:11 +0200

    VAE implementation

---
+++ generatywne/utils.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn as nn
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader
+
+from vae import VAE
+
+
+def corrupt(x, amount):
+    """Corrupt the input x by mixing it with noise according to amount."""
+    noise = torch.rand_like(x)
+    amount = amount.view(-1, 1, 1, 1)
+    return x * (1 - amount) + noise * amount
+
+def vae_loss(
+    x_out: torch.tensor, x: torch.tensor, mean_: torch.tensor, log_std_: torch.tensor
+) -> torch.tensor:
+    mse = nn.functional.mse_loss(x_out, x, reduction="sum")
+    kld = -0.5 * torch.sum(1 + log_std_ - mean_.pow(2) - log_std_.exp())
+    return mse + kld
+
+
+def train(
+    model: VAE,
+    optimizer: Optimizer,
+    loader: DataLoader,
+    losses_list: list,
+    criterion: callable = vae_loss,
+    overfit_on_single_batch: bool = False,
+    device: str = "cuda",
+    epochs: int = 10,
+    show_every: int = 1,
+):
+    for epoch in range(1, epochs + 1):
+        losses_epoch = []
+        for x, _ in loader:
+            x = x.to(device)
+            optimizer.zero_grad()
+            x_out, mean_, log_std_ = model(x)
+            loss = criterion(x_out, x, mean_, log_std_)
+            if not torch.isnan(loss):
+                loss.backward()
+                losses_epoch.append(loss.item())
+                optimizer.step()
+            if overfit_on_single_batch:
+                break
+        losses_list += losses_epoch
+        if epoch and not epoch % show_every:
+            print(f"[{epoch}/{epochs}], Loss: {torch.mean(torch.tensor(losses_epoch)).item():.3}")
+
+
+def generate_random_images(
+    model: VAE,
+    num_imgs: int = 1000,
+    single_img_shape: tuple = (3, 32, 32),
+    device: str = "cuda",
+) -> torch.tensor:
+    imgs = model.decoder(torch.randn([num_imgs, model.latent_dim]).to(device))
+    assert imgs.shape == tuple((num_imgs, *single_img_shape))
+    return imgs
+
+
+def save_images(imgs: torch.tensor, filename: str) -> None:
+    torch.save(imgs.cpu().detach(), filename)
+
+
+def save_model(model: VAE, filename: str) -> None:
+    torch.save(model.state_dict(), filename)
---
+++ generatywne/vae.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim: int, hidden_dim: int, latent_dim: int) -> None:
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc_mean = nn.Linear(hidden_dim, latent_dim)
+        self.fc_log_std = nn.Linear(hidden_dim, latent_dim)
+        self.lrelu = nn.LeakyReLU(0.3)
+
+    def forward(self, x):
+        x = torch.flatten(x, 1)
+        x = self.lrelu(self.fc1(x))
+        x = self.lrelu(self.fc2(x))
+        mean_ = self.fc_mean(x)
+        log_std_ = self.fc_log_std(x)
+        return mean_, log_std_
+
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dim: int, hidden_dim: int, output_dim: int) -> None:
+        super().__init__()
+        self.fc1 = nn.Linear(latent_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, output_dim)
+        self.lrelu = nn.LeakyReLU(0.3)
+
+    def forward(self, x):
+        x = self.lrelu(self.fc1(x))
+        x = self.lrelu(self.fc2(x))
+        x = torch.sigmoid(self.fc3(x))
+        x = x.view((-1, 3, 32, 32))
+        return x
+
+
+class VAE(nn.Module):
+    def __init__(
+        self, input_dim: int, hidden_dim: int, latent_dim: int, output_dim: int
+    ) -> None:
+        super().__init__()
+        self.latent_dim = latent_dim
+        self.encoder = Encoder(input_dim, hidden_dim, latent_dim)
+        self.decoder = Decoder(latent_dim, hidden_dim, output_dim)
+
+    def reparametrization_trick(self, mean_, log_std_):
+        eps = torch.normal(torch.zeros_like(mean_), torch.ones_like(log_std_))
+        z = mean_ + eps * torch.exp(0.5 * log_std_)
+        return z
+
+    def forward(self, x):
+        mean_, log_std_ = self.encoder(x)
+        z = self.reparametrization_trick(mean_, log_std_)
+        x = self.decoder(z)
+        return x, mean_, log_std_
